336fea8fbce67add13d106f4fe9678645ff8020c
Bayu Ramadhan Yunarko <bayuryunarko@gmail.com>
2026-09-09 20:46:18 +0700

    update environtment/dev/main.tf : refactoring disk and labels

--- environtments/dev/main.tf
+++ environtments/dev/main.tf
@@ -10,7 +10,10 @@ variable "machine_type" {}
 variable "boot_disk_image" {}
 variable "boot_disk_type" {}
 variable "boot_disk_size" {}
+variable "boot_disk_labels" {}
 variable "desired_status" {}
+variable "additional_disks" {}
+variable "labels" {}
 
 provider "google" {
   project = var.project_id
@@ -41,10 +44,17 @@ module "compute" {
   zone = var.zone
   desired_status = var.desired_status
 
+  # Lempar map disk tambahan
+  additional_disks = var.additional_disks
+
   boot_disk_image = var.boot_disk_image
   boot_disk_type = var.boot_disk_type
   boot_disk_size = var.boot_disk_size 
+  boot_disk_labels = var.boot_disk_labels
 
 
   subnet_id = module.networking.subnet_id
+
+  labels = var.labels
 }
+
